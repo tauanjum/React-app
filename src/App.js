981f121a72b178dcf6f9678317ba1dc8e6db4139
Ganeshrockz <ganeshrockz@github.com>
2022-05-16 23:22:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			message: ""
+		};
+	}
+
+  componentDidMount() {
+    // Enter the url of your heroku node app
+		fetch(`<url-of-your-heroku-app>`)
+		  .then(res => res.json())
+      .then(json => this.setState({ message: json['msg'] }));
+	}
+
+  render() {    
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-welcome-message">
+          {this.state.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
